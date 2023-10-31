69e4b71363c038e6de15be6187e67cc5388ee481
5P453C0VV80Y <langen.z@ya.ru>
2023-10-31 17:10:29 +0700

    a few tasks

---
+++ lab 2/hello.py
@@ -0,0 +1,56 @@
+# Задача 1
+print (37/10)
+print (37//10)
+print (37%10)
+print('-----------------------------------------')
+# Задача 2
+print ('hello world')
+print('-----------------------------------------')
+
+#Задача 3
+a = 3
+print(type(a))
+a = 3.5
+print(type(a))
+a = 'qwerty'
+print(type(a))
+a = True
+print(type(a))
+a = '123'
+print(type(a))
+print('-----------------------------------------')
+#если попробовать сложить строку с числом -
+#будет ошибка типа
+
+#Задача 4
+asd = int(5.7)
+print(asd)
+asdf = int(-5.7)
+print(asdf)
+asdfg = int(float(3**39))
+print(asdfg)
+print('-----------------------------------------')
+#получилось очень большое число (????)
+
+#Задача 5
+a = str(input('enter your name:'))
+print('howdy,',a)
+print('-----------------------------------------')
+
+#задача 6
+print('how many hours did the doctor drive to work?')
+x = int(input())
+print('how many minutes did the doctor walk from lunch to work?')
+y = int(input())
+print('minutes spent on the road:', ((x*60)+y))
+
+#задача 7
+a = False
+b = True
+c = False
+
+z = (not (a or b) and c)
+print(z)
+
+#задача 8
+
